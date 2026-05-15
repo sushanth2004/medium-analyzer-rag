681a2e54e6af9fe23f0b53cac06d766149bbf312
Sushanth Neelam <neelamsushanth2004@gmail.com>
2026-05-15 10:47:15 +0530

    implemented ingestion

--- ingestion.py
+++ ingestion.py
@@ -1,11 +1,54 @@
-import os
+# Ingesting the medium blog into our vector store
+# 1. Load - TextLoader
+# 2. split - TextSplitter
+# 3. embed - OpenAIEmbeddings 
+# 4. store - PineconeVectorStore
 
+import os
 from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
 
 load_dotenv()
+
+def main():
+    print("Ingesting....")
+
+    # 1. load
+    print("Loading the data...")
+    loader = TextLoader("mediumblog1.txt", encoding='utf-8')
+    document = loader.load()
+    # print(document[0].page_content)
+    
+    # 2. split
+    print("splitting...")
+    text_splitter = CharacterTextSplitter(
+        chunk_size = 1000,
+        chunk_overlap = 0
+    )
+    texts = text_splitter.split_documents(document)
+    # print(texts[0])
+    print(f"created {len(texts)} chunks")
+
+    # 3.embed
+    print("embedding...")
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+
+    # 4.store
+    print("storing the embeddings...")
+    PineconeVectorStore.from_documents(
+        texts,
+        embeddings,
+        index_name = os.environ["INDEX_NAME"]
+    )
+    print("Finish")
+    
+
 
 
 
 if __name__ == '__main__':
-    print("Ingesting....")
+    main()
     
